6737e77b3ae98d1e2342db68b28a13cf43764133
dhart31 <derekjordanhart@gmail.com>
2025-03-10 21:31:48 -0400

    Linted minhash script

--- minhash.py
+++ minhash.py
@@ -1,19 +1,36 @@
+"""
+Author: Derek Hart
+Date: 03/10/2025
+Creates sequence sketch of sequences
+"""
+import heapq
 import mmh3
 import screed
-import heapq
 import numpy as np
 
-tab = str.maketrans("ACTG", "TGAC")
 def reverse_complement(seq):
+    """
+    Returns reverse complement string of DNA sequence
+    """
+    tab = str.maketrans("ACTG", "TGAC")
     return seq.translate(tab)[::-1]
 
 MAX_HASH = 2**64
 
 def hash_kmer(kmer):
+    """
+    Calculates a 64-bit unsigned hash for input kmer
+    """
     hash_val = mmh3.hash64(kmer, 42, signed=False)[0]
     return hash_val
 
-def sketch_sequence(sequence, kmer_size=21, sampling_factor=1000):
+def sketch_sequence(sequence, kmer_size=21, max_hashes=1000):
+    """
+    Creates a representative sketch of sequence with hash array
+    - keeps smallest hash values
+    - array is can be no larger than max_hashes
+    - returns numpy array
+    """
     hash_heap = []
     rc_seq = reverse_complement(sequence)
 
@@ -29,7 +46,7 @@ def sketch_sequence(sequence, kmer_size=21, sampling_factor=1000):
                 )
             )
 
-        if len(hash_heap) < sampling_factor:
+        if len(hash_heap) < max_hashes:
             heapq.heappush(hash_heap, -hash_val)
         else:
             if -hash_heap[0] > hash_val:
